ac670924e23213cb3ab73ac57ac9923abe6b838a
Mostafizur Rahman <mostafizurhimself@gmail.com>
2025-02-20 15:06:39 +0600

    feat: remove unused files

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import React from "react";
-import { Builder, Header } from "@repo/builder";
-import { Button } from "@repo/builder";
+import { Builder } from "@repo/builder";
 import "@repo/builder/dist/style.css";
 
 export default function HomePage() {
@@ -10,10 +8,7 @@ export default function HomePage() {
     <div>
       {/* <Header text="Page Builder" />
       <Button>Click me</Button> */}
-      <Builder
-        content={{}}
-        theme={{ name: "Storefront", id: 1, settings: {} }}
-      />
+      <Builder content={{}} />
     </div>
   );
 }
--- packages/builder/src/index.ts
+++ packages/builder/src/index.ts
@@ -1,5 +1,3 @@
 import "./index.css";
 
-export { Button } from "./Button";
-export { Header } from "./Header";
 export { default as Builder } from "./components/builder";
